7c24d3a89f65b301b77dea305086420f147c505a
buront11 <ysk.s_0220@outlook.com>
2021-05-24 19:24:53 +0900

    StratifiedKFoldを実装

--- train_base.py
+++ train_base.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataset import Subset
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold
 
 from tqdm import tqdm
 from collections import OrderedDict
@@ -54,6 +54,33 @@ class TrainBase():
         print("valid loss: {}".format(self.valid_loss))
         print("valid acc : {}".format(self.valid_correct/self.valid_total))
 
+    def stratified_kfold(self, dataset, batch_size, epoch, study_func, n_splits=2):
+        skf = StratifiedKFold(n_splits)
+
+        for _fold, (train_index, valid_index) in enumerate(skf.split(dataset)):
+            train_dataset = Subset(dataset, train_index)
+            train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True, drop_last=True)
+            valid_dataset   = Subset(dataset, valid_index)
+            valid_dataloader = DataLoader(valid_dataset, batch_size, shuffle=False, drop_last=True)
+
+            self.init_param()
+            
+            # そのうちtrainとvalidで別関数化する
+            study_func(train_dataloader, valid_dataloader)
+
+            self.train_loss += self.train_loss / skf.n_splits
+            self.train_correct += self.train_correct / skf.n_splits
+            self.train_total += self.train_total / skf.n_splits
+            self.valid_loss += self.valid_loss / skf.n_splits
+            self.valid_correct += self.valid_correct / skf.n_splits
+            self.valid_total += self.valid_total / skf.n_splits
+
+        print("epoch : [{}/{}]".format(epoch+1, self.epochs))
+        print("train loss: {}".format(self.train_loss))
+        print("train acc : {}".format(self.train_correct/self.train_total))
+        print("valid loss: {}".format(self.valid_loss))
+        print("valid acc : {}".format(self.valid_correct/self.valid_total))
+
     def init_param(self):
         self.train_loss = 0
         self.valid_loss = 0
@@ -82,10 +109,11 @@ class ClassifierTrain(TrainBase):
         for epoch in range(self.epochs):
             self.kfold_cross_validation(self.dataset, self.batch_size, epoch, self.study)
 
-            early_stopping(self.train_loss, self.model)
-            if early_stopping.early_stop:
-                print("Early Stopping!")
-                break
+            if early_stopping is not None:
+                early_stopping(self.train_loss, self.model)
+                if early_stopping.early_stop:
+                    print("Early Stopping!")
+                    break
 
     def study(self, train_dataloader, valid_dataloader):
 
