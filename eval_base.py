2b60b425ce32c5720da765c733954cfe36b31687
buront11 <ysk.s_0220@outlook.com>
2021-05-24 15:07:36 +0900

    eval baseを実装

--- eval_base.py
+++ eval_base.py
@@ -1,3 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+class EvalBase():
+    def __init__(self, model, model_path):
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model = model
+        self.model.load_state_dict(torch.load(model_path, map_location=self.device))
